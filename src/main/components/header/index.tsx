afb01fee41fe36f9d0915f1aa89e1607bd9a0b36
gzhel <gregoryzhel@gmail.com>
2022-09-22 20:35:53 +0300

    feature/sneakers-8 - header mobile menu

--- src/main/components/header/index.tsx
+++ src/main/components/header/index.tsx
@@ -1,6 +1,6 @@
 // @ts-ignore
 import * as Icons from '@iconscout/react-unicons';
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import s from './index.module.scss';
 import { useResizeDetector } from 'react-resize-detector';
 
@@ -17,18 +17,61 @@ export const Header: FC = () => {
 
   const isMobile = useMemo<boolean>(() => (width || 0) < 400, [width]);
 
+  const [isMenuCollapsed, setMenuCollapsed] = useState(false);
+  const handleMenuCollapsed = () => setMenuCollapsed((prev) => !prev);
+
   return (
-    <header className={s.header}>
-      <div className={s.left}>
-        {!isMobile ? null : (
-          <div className={s.menu}>
-            <Icons.UilBars size="2em" color="var(--neutral-color-2)" />
+    <>
+      <header className={s.header}>
+        <div className={s.left}>
+          {!isMobile ? null : (
+            <div className={s.menu} onClick={handleMenuCollapsed}>
+              <Icons.UilBars
+                size="2em"
+                color={isMenuCollapsed ? 'var(--neutral-color-2)' : 'var(--primary-color-1)'}
+              />
+            </div>
+          )}
+          <div className={s.logo}>
+            <a href="#">sneakers</a>
           </div>
-        )}
-        <div className={s.logo}>
-          <a href="#">sneakers</a>
+          {isMobile ? null : (
+            <nav>
+              <ul className={s.nav}>
+                <li className={s.navItem}>
+                  <a href="#">Collections</a>
+                </li>
+                <li className={s.navItem}>
+                  <a href="#">Men</a>
+                </li>
+                <li className={s.navItem}>
+                  <a href="#">Women</a>
+                </li>
+                <li className={s.navItem}>
+                  <a href="#">About</a>
+                </li>
+                <li className={s.navItem}>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </div>
-        {isMobile ? null : (
+
+        <div className={s.right}>
+          <div className={s.cart} onClick={alertCartValue}>
+            <Icons.UilShoppingCart
+              size={isMobile ? '1.5em' : '2em'}
+              color="var(--neutral-color-2)"
+            />
+          </div>
+          <a href="#" className={s.profile}>
+            <Icons.UilUserCircle size={isMobile ? '2em' : '3em'} color="var(--neutral-color-1)" />
+          </a>
+        </div>
+      </header>
+      {!isMobile || isMenuCollapsed ? null : (
+        <div className={s.mobileMenu}>
           <nav>
             <ul className={s.nav}>
               <li className={s.navItem}>
@@ -48,17 +91,8 @@ export const Header: FC = () => {
               </li>
             </ul>
           </nav>
-        )}
-      </div>
-
-      <div className={s.right}>
-        <div className={s.cart} onClick={alertCartValue}>
-          <Icons.UilShoppingCart size={isMobile ? '1.5em' : '2em'} color="var(--neutral-color-2)" />
         </div>
-        <a href="#" className={s.profile}>
-          <Icons.UilUserCircle size={isMobile ? '2em' : '3em'} color="var(--neutral-color-1)" />
-        </a>
-      </div>
-    </header>
+      )}
+    </>
   );
 };
